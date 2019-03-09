40558a861a5fdfa57e2cb0492e0e6b5d28c732e5
dwAdam <adamcooper.w@gmail.com>
2019-03-10 00:24:31 +0100

    Security config, WebMvc config

---
+++ configuration/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.sbsse.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
+		return bcpe;
+	}
+
+}
